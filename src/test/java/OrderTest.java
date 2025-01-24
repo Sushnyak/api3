7b2390036c24cf13c90dabd547b6071349bc5d3d
Daniil Zakharov <danilazakharov2003@gmail.com>
2025-01-24 10:32:36 +0300

    add testClass

---
+++ src/test/java/OrderTest.java
@@ -0,0 +1,45 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+public class OrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        //    options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldSuccessForm() {
+        List<WebElement> inputs = driver.findElements(By.cssSelector(".input__control"));
+        inputs.get(0).sendKeys("Захаров Даниил");
+        inputs.get(1).sendKeys("+79154230549");
+        driver.findElement(By.cssSelector("data-test-id='agreement'"));
+    }
+}
